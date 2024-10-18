29eaad468cbb03c8bcca212636b609821e867c35
Edwin unity <eapaniagua8@misena.edu.co>
2024-10-17 19:38:20 -0500

    Caminos en el agua

---
+++ Assets/Scripts/Hojas.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hojas : MonoBehaviour
+{
+
+    public GameObject hoja;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LanzarRayo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LanzarRayo()
+    {
+        RaycastHit hit;
+        Ray rayo = new Ray(transform.position + Vector3.up * 3 - Vector3.forward, Vector3.down);
+
+        if (Physics.Raycast(rayo, out hit))
+        {
+            if (hit.collider.CompareTag("Agua"))
+            {
+                Instantiate(hoja, transform.position - Vector3.forward, transform.rotation);
+            }
+            else if(hit.collider.CompareTag("obstaculo"))
+            {
+               Destroy(hit.transform.gameObject);
+            }
+        }
+        
+    }
+}
